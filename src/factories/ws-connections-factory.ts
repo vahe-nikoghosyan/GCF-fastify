4d67ed8769a309fd1e0e5b86d915eced365cf481
Hayk Manandyan <haykmanandyan@doublecoconut.com>
2023-07-24 18:43:28 +0400

    fix: handling ws connection repository calls

--- src/factories/ws-connections-factory.ts
+++ src/factories/ws-connections-factory.ts
@@ -5,22 +5,9 @@ import {
   saveWSConnectionWithSpecificId,
 } from "../repositories/ws-connection-repository";
 import { UpdateWSConnectionRequestBody } from "../@types/ws-connection";
-import logger from "../logger";
 
-const log = logger.child({ from: "WS Connections Factory" });
-
-export const getWSConnectionById = async (id: string) => {
-  try {
-    const wsConnection = await findWSConnectionById(id);
-    if (wsConnection == null) {
-      return null;
-    }
-    return wsConnection;
-  } catch (error) {
-    log.error("Error retrieving user:", error);
-    return null;
-  }
-};
+export const getWSConnectionById = async (id: string) =>
+  findWSConnectionById(id);
 
 export const createWSConnection = async (id: string) =>
   saveWSConnectionWithSpecificId({ id });
@@ -28,30 +15,7 @@ export const createWSConnection = async (id: string) =>
 export const updateWSConnection = async (
   id: string,
   body: UpdateWSConnectionRequestBody,
-) => {
-  try {
-    const wsConnection = await getWSConnectionById(id);
-    if (wsConnection == null) {
-      return null;
-    }
-    await modifyWSConnectionById(id, body);
-    return true;
-  } catch (error) {
-    log.error("Error updating ws connection:", error);
-    return null;
-  }
-};
+) => modifyWSConnectionById(id, body);
 
-export const deleteWSConnectionById = async (id: string) => {
-  try {
-    const wsConnection = await getWSConnectionById(id);
-    if (wsConnection == null) {
-      return null;
-    }
-    await removeWSConnectionById(id);
-    return true;
-  } catch (error) {
-    log.error("Error deleting ws connection:", error);
-    return null;
-  }
-};
+export const deleteWSConnectionById = async (id: string) =>
+  removeWSConnectionById(id);
