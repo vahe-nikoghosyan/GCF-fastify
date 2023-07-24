60cc14835b0ce52979189f185eb51caff6a9e74f
karo-dc <karo.ghazarian@gmail.com>
2023-07-24 15:34:03 +0400

    chore: ws d ts place

--- src/@types/ws.d.ts
+++ src/@types/ws.d.ts
@@ -1,16 +1,7 @@
-export type WSAction = "PING" | "HANDSHAKE";
-
-export type Action = "CONNECTION";
-
-export type WSActionType = "ERROR" | "SUCCESS" | "CONFIRM";
-
 export interface WSMessage<T = WSHeader, R = WSBody> {
   header: T;
   body: R;
 }
-
-export type WSRequestMessage = WSMessage<WSRequestHeader, WSBody>;
-export type WSResponseMessage = WSMessage<WSResponseHeader, WSBody>;
 
 export interface WSHeader {
   action: WSAction;
@@ -38,3 +29,9 @@ export interface WSRequestBody {
 export interface WSResponseBody {
   payload: string;
 }
+
+export type WSRequestMessage = WSMessage<WSRequestHeader, WSBody>;
+export type WSResponseMessage = WSMessage<WSResponseHeader, WSBody>;
+
+export type WSAction = "PING" | "HANDSHAKE";
+export type WSActionType = "ERROR" | "SUCCESS" | "CONFIRM";
