fe9e079de686a9f69d91253a819a37c601d0030e
vahe-dc <vahenikoghosyan@doublecoconut.com>
2023-07-25 17:27:44 +0400

    feat: add spin logic

--- src/factories/ws-factory.ts
+++ src/factories/ws-factory.ts
@@ -73,23 +73,67 @@ export const onHandshake = async (
   });
 };
 
-function getRandomSymbol(dynamicSymbol?: string) {
-  const symbols = ["attack", "coin", "shield", "purse", "spin", "raid"];
+function getRandomSymbol(
+  betX: number,
+  dynamicSymbol?: { name: string; percent: number },
+) {
+  const symbols = [
+    {
+      name: "attack",
+      percent: 10,
+    },
+    {
+      name: "coin",
+      percent: 29,
+    },
+    {
+      name: "shield",
+      percent: 15,
+    },
+    {
+      name: "purse",
+      percent: 9,
+    },
+    {
+      name: "spin",
+      percent: 57,
+    },
+    {
+      name: "raid",
+      percent: 100,
+    },
+  ];
   if (dynamicSymbol) {
     symbols.push(dynamicSymbol);
   }
-  const randomIndex = Math.floor(Math.random() * symbols.length);
-  return symbols[randomIndex];
+
+  const totalPercent = symbols.reduce((acc, cur) => {
+    acc += cur.percent;
+    return acc;
+  }, 0);
+
+  let gago = null;
+  let randomPercent = Math.floor(Math.random() * totalPercent);
+  symbols.some((symbol) => {
+    randomPercent -= symbol.percent;
+    if (randomPercent <= 0) {
+      gago = symbol.name;
+      return true;
+    }
+    return false;
+  });
+  // const randomIndex = Math.floor(Math.random() * symbols.length);
+  return gago;
 }
 
 export const spin = async (
   connection: SocketStream,
   header: WSRequestHeader,
-  _: any,
+  body: any,
 ) => {
   const results = [];
   for (let i = 0; i < 3; i++) {
-    results.push(getRandomSymbol());
+    results.push(getRandomSymbol(body.betX));
   }
   return sendWSMessage(
     connection,
